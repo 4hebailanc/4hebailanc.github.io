c43c978b2c9abecab6c2e230847a0d099949da89
bailanhe <55342825+bailanhe@users.noreply.github.com>
2025-10-07 11:35:23 +0000

    Deploying to gh-pages from @ bailanhe/bailanhe.github.io@5436f7e92e468dc516ec8dbe340f0ad36c6ccbae 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "4hebailanc/4hebailanc.github.io",
+    "data-repo": "bailanhe/bailanhe.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
